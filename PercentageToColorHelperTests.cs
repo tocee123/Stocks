e24ad56b760ae7afc2d5d144d4d84e6c47eb5e15
Tamas Torok <tamas.torok@accenture.com>
2021-10-21 15:07:39 +0200

    Fixing tests

--- PercentageToColorHelperTests.cs
+++ PercentageToColorHelperTests.cs
@@ -8,22 +8,46 @@ namespace WebDownloading.Test
     [TestFixture]
     public class PercentageToColorHelperTests
     {
-        [TestCaseSource(typeof(GetFontColorByDividendToPriceSource), nameof(GetFontColorByDividendToPriceTestCases.Source))]
-        public void GetFontColorByDividendToPrice_WhenRatioIsGiven_ReturnsColor(GetFontColorByDividendToPriceSource testCase)
+        [Test, TestCaseSource(nameof(GetFontColorByDividendToPriceSource))]
+        public void GetFontColorByDividendToPrice_WhenRatioIsGiven_ReturnsColor(double ratio, Color expectedColor)
         {
-            var result = testCase.Ratio.GetFontColorByDividendToPrice();
-            Assert.AreEqual(testCase.ExpectedColor, result);
+            var result = ratio.GetFontColorByDividendToPrice();
+            Assert.AreEqual(expectedColor, result);
         }
 
-        public class GetFontColorByDividendToPriceSource
+        private static IEnumerable<TestCaseData> GetFontColorByDividendToPriceSource
         {
-            public double Ratio { get; set; }
-            public Color ExpectedColor { get; set; }
+            get
+            {
+                yield return new TestCaseData(0.021, Color.White);
+                yield return new TestCaseData(0.016, Color.White);
+                yield return new TestCaseData(0.02, Color.White);
+                yield return new TestCaseData(0.005, Color.White);
+                yield return new TestCaseData(0.004, Color.White);
+                yield return new TestCaseData(0.014, Color.Black);
+                yield return new TestCaseData(0.006, Color.Black);
+            }
         }
 
-        private class GetFontColorByDividendToPriceTestCases
+        [Test, TestCaseSource(nameof(GetBackgroundColorByDividendToPriceSource))]
+        public void GetBackgroundColorByDividendToPrice_WhenRatioIsGiven_ReturnsColor(double ratio, Color expectedColor)
         {
-            public static IEnumerable<GetFontColorByDividendToPriceSource> Source = new[] { new GetFontColorByDividendToPriceSource { Ratio = 0.021, ExpectedColor = Color.White}  };
+            var result = ratio.GetBackgroundColorByDividendToPrice();
+            Assert.AreEqual(expectedColor, result);
+        }
+
+        private static IEnumerable<TestCaseData> GetBackgroundColorByDividendToPriceSource
+        {
+            get
+            {
+                yield return new TestCaseData(0.021, Color.Green);
+                yield return new TestCaseData(0.016, Color.DarkBlue);
+                yield return new TestCaseData(0.02, Color.DarkBlue);
+                yield return new TestCaseData(0.005, Color.Red);
+                yield return new TestCaseData(0.004, Color.Red);
+                yield return new TestCaseData(0.014, Color.LightBlue);
+                yield return new TestCaseData(0.006, Color.Yellow);
+            }
         }
     }
 }
