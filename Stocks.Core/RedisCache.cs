348b4a18f671f7051ca3e4e099ef8220ddb95bd8
Tamas Torok <tamas.torok@accenture.com>
2021-10-23 22:35:07 +0200

    Cashe for Tony

--- Stocks.Core/RedisCache.cs
+++ Stocks.Core/RedisCache.cs
@@ -47,6 +47,8 @@ namespace Stocks.Core
             "PBCT", "TDS", "FLIC", "WEYS", "ORI", "T", "BEN", "MDU", "EBTC", "UBSI", "MATW", "SBSI", "AROW", "NJR", "ENB", "ARTN.A", "THFF", "CTBI", "OZK", "HRL", "UGI", "SRCE", "NNN", "LEG", "ABM", "WBA", "UVV", "WTRG", "NWN", "NFG", "CAH", "BRC", "XOM", "AFL", "BANF", "KO", "WABC", "JW.A", "BRO", "UHT", "MCY", "ADM", "SEIC", "CWT", "SON", "DCI", "FUL", "CBSH", "SJW", "MGRC", "BKH", "O", "AOS", "BF.B", "EV", "CBU", "TNC", "ED", "CL", "SYY", "TMP", "MGEE", "RPM", "RTX", "CHD", "FELE", "NEE", "MKC.V", "UMBF", "AWR", "CVX", "ATO", "EMR", "BMI", "RLI", "CFR", "NUE", "SCL", "PII", "TRI", "CNI", "CINF", "FRT", "EXPD", "GPC", "MDT", "ATR", "LECO", "IBM", "KMB", "PG", "WMT", "RNR", "PPG", "PEP", "MSA", "CLX", "JNJ", "DOV", "LANC", "JKHY", "ERIE", "CB", "SWK", "MMM", "GD", "LOW", "ADP", "CAT", "CSL", "TROW", "ECL", "ITW", "MCD", "ALB", "NDSN", "TGT", "BDX", "APD", "SYK", "PH", "SHW", "LIN", "ESS", "CTAS", "GWW", "SPGI", "WST", "ROP"
             };
 
+            //var StocksOfInterest = new[] { "T", "BAX", "KMB", "K", "IBM", "DLR", "KO", "JNJ", "MO", "PG", "WMT", "PEP", "HAS", "PM", "MMM", "BA", "WDC", "CVX", " MCD", "XOM", "RNO.PA", "CBRL", "DIS", "BLK", "TROW", "AXP", "AAPL", "COP", "EBAY", "TGT", "SPG", "JCI", "OXI", "GPS", "APA" };
+
             var convertedValue = JsonConvert.SerializeObject(StocksOfInterest);
             using var cm = ConnectionMultiplexer.Connect(_redisConnectionString);
             var db = cm.GetDatabase();
