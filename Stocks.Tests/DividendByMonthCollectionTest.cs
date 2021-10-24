41bb81123277729bc412a1c905312cdf37fb87d8
Tamas Torok <tamas.torok@accenture.com>
2021-10-24 11:07:06 +0200

    changed logic

--- Stocks.Tests/DividendByMonthCollectionTest.cs
+++ Stocks.Tests/DividendByMonthCollectionTest.cs
@@ -4,7 +4,9 @@ using Stocks.Core.Models;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace WebDownloading.Test
@@ -23,9 +25,7 @@ namespace WebDownloading.Test
         [Test]
         public async Task SelectTop1DividnedByMonth()
         {
-            var year = 2021;
-
-            var stocks = (await _target.GetStocks()).SelectMany(s => s.DividendHistories, (s, dh) => new { s.Name, dh.ExDate, DividendToPrice = Math.Round(dh.Amount / s.Price, 4), s.Price, dh.Amount, s.ShortName }).Where(s => s.ExDate.Year == year);
+            var stocks = (await _target.GetStocks()).SelectMany(s => s.DividendHistories, (s, dh) => new { s.Name, dh.ExDate, DividendToPrice = Math.Round(dh.Amount / s.Price, 4), s.Price, dh.Amount, s.ShortName });
 
             var grouppedByMonth = stocks.GroupBy(s => new { s.ExDate.Month, s.ExDate.Year }).Select(s => new { s.Key, TopDividendPayer = s.OrderByDescending(so => so.DividendToPrice).FirstOrDefault() });
             Console.WriteLine("Monthly best ratios:");
@@ -34,13 +34,15 @@ namespace WebDownloading.Test
             Console.WriteLine($"if you would invest {investment}$, you would get:");
             int calculateStockCountFromPrice(double price) => (int)(investment / price);
             var sum = 0.0;
-            foreach (var item in grouppedByMonth)
+            var sb = new StringBuilder();
+            foreach (var item in grouppedByMonth.OrderByDescending(x => x.Key.Year).ThenByDescending(x => x.Key.Month))
             {
                 var amountOfDividend = Math.Round(calculateStockCountFromPrice(item.TopDividendPayer.Price) * item.TopDividendPayer.Amount, 2);
                 sum += amountOfDividend;
-                Console.WriteLine($"In {item.Key.Year}/{item.Key.Month}: {item.TopDividendPayer.Name} '{item.TopDividendPayer.ShortName}' with {item.TopDividendPayer.Price}$, dividend {item.TopDividendPayer.Amount}$ ratio: {item.TopDividendPayer.DividendToPrice:0.00%} you could buy {calculateStockCountFromPrice(item.TopDividendPayer.Price)} stocks and get {amountOfDividend}$ on { item.TopDividendPayer.ExDate.ToShortDateString()}");
+                sb.AppendLine($"In {item.Key.Year}/{item.Key.Month}: {item.TopDividendPayer.Name} '{item.TopDividendPayer.ShortName}' with {item.TopDividendPayer.Price}$, dividend {item.TopDividendPayer.Amount}$ ratio: {item.TopDividendPayer.DividendToPrice:0.00%} you could buy {calculateStockCountFromPrice(item.TopDividendPayer.Price)} stocks and get {amountOfDividend}$ on { item.TopDividendPayer.ExDate.ToShortDateString()}");
             }
-            Console.WriteLine($"{sum}$");
+            sb.AppendLine($"{sum}$");
+            File.WriteAllText(@"c:\temp\test111.txt", sb.ToString());
         }
     }
 }
