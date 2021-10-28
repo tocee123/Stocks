be1892b8ee15793977c121469ca27e2cbfedbab7
SFPXV0O <SFPXV0O@post.at>
2021-10-28 19:36:45 +0000

    DateCalculator

---
+++ Stocks.Core/DateCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Stocks.Core
+{
+    public class DateCalculator
+    {
+        private static int _yesterday = -1;
+        private static int _tomorrow = 1;
+
+        public static DateTime CalculateWhenToBuy(DateTime date)
+        => MoveDayOutFromWeekend(date, _yesterday);
+
+        public static DateTime CalculateWhenToSell(DateTime date)
+        => MoveDayOutFromWeekend(date, _tomorrow);
+
+        private static DateTime MoveDayOutFromWeekend(DateTime date, int daysToMove)
+        {
+            DateTime result = date.AddDays(daysToMove);
+            while (IsWeekend(result))
+                result = result.AddDays(daysToMove);
+            return result;
+        }
+
+        readonly static DayOfWeek[] _weekendDays = new[] { DayOfWeek.Sunday, DayOfWeek.Saturday };
+
+        private static bool IsWeekend(DateTime date)
+            => _weekendDays.Any(wd => wd == date.DayOfWeek);
+    }
+}
---
+++ Stocks.Tests/DateCalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using Stocks.Core;
+using System;
+using System.Collections.Generic;
+
+namespace WebDownloading.Test
+{
+    [TestFixture]
+    public class DateCalculatorTests
+    {
+        DateCalculator _target = new();
+
+        [TestCaseSource(nameof(WeekendDays))]
+        public void CalculateWhenToBuy_WhenExDateIsWeekend_ReturnsFriday(DateTime date)
+        {
+            var result = _target.CalculateWhenToBuy(date);
+            Assert.AreEqual(DayOfWeek.Friday, result.DayOfWeek);
+        }
+
+        private static IEnumerable<DateTime> WeekendDays
+        {
+            get
+            {
+                yield return new DateTime(2021, 1, 2);
+                yield return new DateTime(2021, 1, 3);
+            }
+        }
+
+        [TestCaseSource(nameof(WeekendDays))]
+        public void CalculateWhenToSell_WhenExDateIsWeekend_ReturnsMonday(DateTime date)
+        {
+            var result = _target.CalculateWhenToSell(date);
+            Assert.AreEqual(DayOfWeek.Monday, result.DayOfWeek);
+        }
+        private static IEnumerable<DateTime> CalculateWhenToBuy_WhenExDateIsWeekend_ReturnsFriday_Source
+        {
+            get
+            {
+                yield return new DateTime(2021, 1, 2);
+                yield return new DateTime(2021, 1, 3);
+            }
+        }
+    }
+}
