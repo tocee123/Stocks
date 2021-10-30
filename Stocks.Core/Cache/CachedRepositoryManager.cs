ea83a907de78e622442590b63c030ae54e9fefbc
SFPXV0O <SFPXV0O@post.at>
2021-10-30 10:52:29 +0000

    CachedRepositoryManager created

---
+++ Stocks.Core/Cache/CachedRepositoryManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stocks.Core.Cache
+{
+    public class CachedRepositoryManager
+    {
+        internal IEnumerable<string> GetStocksOfInterest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
