f071c5f091c8bab90c0172963f6ca758a7ecc275
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-06-11 19:49:44 +0200

    Update ExcelSaverTests.cs

--- Stocks.Tests/ExcelSaverTests.cs
+++ Stocks.Tests/ExcelSaverTests.cs
@@ -25,27 +25,27 @@ namespace WebDownloading.Test
 
         //TODO
         //[Test]
-        public void SaveToExcel_WhenNotEmptyArrayIsGiven_ReturnsExcelFile()
-        {
-            var fixture = new Fixture();
+        //public void SaveToExcel_WhenNotEmptyArrayIsGiven_ReturnsExcelFile()
+        //{
+        //    var fixture = new Fixture();
 
-            var stockDividends = fixture.Create<IEnumerable<StockDividend>>();
-            var bytes = _target.SaveToExcel(stockDividends);
-            var fileName = @"C:\temp\test1.xlsx";
-            File.WriteAllBytes(fileName, bytes);
-            Assert.IsTrue(File.Exists(fileName));
-        }
+        //    var stockDividends = fixture.Create<IEnumerable<StockDividend>>();
+        //    var bytes = _target.SaveToExcel(stockDividends);
+        //    var fileName = @"C:\temp\test1.xlsx";
+        //    File.WriteAllBytes(fileName, bytes);
+        //    Assert.IsTrue(File.Exists(fileName));
+        //}
 
-        //TODO
-        //[Test]
-        public async Task SaveReal()
-        {
-            var configurationSub = Substitute.For<IConfiguration>();
-            var sr = new StocksRepository(new StocksLoader(new StockDividendHistoryLoader()), new StocksOfInterestRespository());
-            var bytes = _target.SaveToExcel(await sr.GetStocksAsync());
-            var fileName = @"C:\temp\test1.xlsx";
-            File.WriteAllBytes(fileName, bytes);
-            Assert.IsTrue(File.Exists(fileName));
-        }
+        ////TODO
+        ////[Test]
+        //public async Task SaveReal()
+        //{
+        //    var configurationSub = Substitute.For<IConfiguration>();
+        //    var sr = new StocksRepository(new StocksLoader(new StockDividendHistoryLoader()), new StocksOfInterestRespository());
+        //    var bytes = _target.SaveToExcel(await sr.GetStocksAsync());
+        //    var fileName = @"C:\temp\test1.xlsx";
+        //    File.WriteAllBytes(fileName, bytes);
+        //    Assert.IsTrue(File.Exists(fileName));
+        //}
     }
 }
