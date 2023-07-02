0ac34c995795bca7b91a14f00db6f2ca296a5089
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-07-02 21:40:35 +0200

    New page added

---
+++ Stocks.Web/Pages/DividendEventDisplay.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Stocks.Web.Pages
+{
+    public class DividendEventDisplayModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
