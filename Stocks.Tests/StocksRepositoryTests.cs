c77e8fb8f86305d641107288851d76024965d483
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-07-02 20:58:27 +0200

    Remvoed test as it is not correct

--- Stocks.Tests/StocksRepositoryTests.cs
+++ Stocks.Tests/StocksRepositoryTests.cs
@@ -26,19 +26,5 @@ namespace WebDownloading.Test
             Assert.IsNotNull(result);
             Assert.IsTrue(result.Any());
         }
-
-        //TODO
-        [Test, Ignore("something is failing")]
-        public async Task GetStocks_()
-        {
-            var result = await _target.GetStocksAsync();
-            Console.WriteLine(result.Count());
-            var groupped = result.GroupBy(s => s.LatestDividendHistory.WhenToBuy).Select(s => s.OrderByDescending(s1 => s1.DividendToPrice).First());
-            result = result.Where(s => s.LatestDividendHistory.WhenToBuy == DateTime.Parse("2021-10-27")).OrderByDescending(s => s.DividendToPrice);
-            foreach (var s in groupped)
-            {
-                Console.WriteLine($"{s.LatestDividendHistory.WhenToBuy}-> {s.Ticker}, {s.DividendToPrice.ToPercentageDisplay()}");
-            }
-        }
     }
 }
