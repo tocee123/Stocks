56e18762b37a48216c5fc37d35f88681fb81b463
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-07-03 17:34:29 +0200

    Cleanup calendargenerator class (#26)
    * Clean up code
    
    * Update margin
    
    * updated workflow
    
    * update
    
    * updated wf
    
    * update css
    
    * update
    
    * commit

--- Stocks.Core/DividendDisplay/DisplayDay.cs
+++ Stocks.Core/DividendDisplay/DisplayDay.cs
@@ -1,2 +1,20 @@
 ﻿namespace Stocks.Core.DividendDisplay;
-public record DisplayDay(int Day, string DayOfWeek, string CardCss, string HeaderCss, IEnumerable<DisplayDividendHistory> DisplayDividendHistories);
+public record DisplayDay(int Day, string DayOfWeek, string CardCss, string HeaderCss, IEnumerable<DisplayDividendHistory> DisplayDividendHistories)
+{
+    public static DisplayDay ToDisplayDay(IDateProvider dateProvider, DateTime d, Dictionary<DateTime, List<DisplayDividendHistory>> dividendHistoriesByDate)
+    => new(d.Day, GetDisplayDateOfWeek(d), GetClassForCard(dateProvider, d), GetClassForHeader(dateProvider, d), dividendHistoriesByDate.GetValueOrDefault(d, new List<DisplayDividendHistory>()));
+
+    private static string GetDisplayDateOfWeek(DateTime day)
+        => day.DayOfWeek.ToString()[..3];
+
+    private static string GetClassForHeader(IDateProvider dateProvider, DateTime date) => date switch
+    {
+        { } when date == dateProvider.GetToday() => "headerToday",
+        { } when date.Month != dateProvider.GetToday().Month => "headerOtherMonth",
+        { } when dateProvider.IsWeekend(date) => "headerWeekend",
+        _ => "header",
+    };
+
+    private static string GetClassForCard(IDateProvider dateProvider, DateTime date)
+    => dateProvider.IsWeekend(date) ? "cardWeekend" : "card";
+}
