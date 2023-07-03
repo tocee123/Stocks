a0cc964c3b77a0a588e606d6c3c87e761555f60a
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-07-03 12:59:07 +0200

    Added interface

--- Stocks.Core/DividendDisplay/CalendarGenerator.cs
+++ Stocks.Core/DividendDisplay/CalendarGenerator.cs
@@ -1,8 +1,34 @@
 ﻿namespace Stocks.Core.DividendDisplay;
-public class CalendarGenerator
+public class CalendarGenerator : ICalendarGenerator
 {
+    IDateProvider _dateTimeProvider;
+
+    public CalendarGenerator(IDateProvider dateTimeProvider)
+    {
+        _dateTimeProvider = dateTimeProvider;
+    }
+
     public IEnumerable<IEnumerable<DisplayDay>> GenerateMonth()
     {
-        return null;
+        var today = _dateTimeProvider.GetToday();
+
+        var startDay = DayOfWeek.Monday;
+        var dayAdjustment = 0;
+
+        var monthsFirstDay = new DateTime(today.Year, today.Month, 1);
+        var displayCalendarsFirstMonday = monthsFirstDay;
+
+        if (displayCalendarsFirstMonday.DayOfWeek != startDay)
+        {
+            dayAdjustment = (int)displayCalendarsFirstMonday.DayOfWeek;
+            displayCalendarsFirstMonday = displayCalendarsFirstMonday.AddDays(-dayAdjustment);
+        }
+        var wholeMonth = Enumerable.Range(0, DateTime.DaysInMonth(monthsFirstDay.Year, monthsFirstDay.Month) + dayAdjustment).Select(i => displayCalendarsFirstMonday.AddDays(i));
+
+        var mondays = wholeMonth.Where(d => d.DayOfWeek == startDay);
+
+        var month = mondays.Select(d => Enumerable.Range(0, 7).Select(i => d.AddDays(i)));
+
+        return month;
     }
 }
---
+++ Stocks.Core/DividendDisplay/ICalendarGenerator.cs
@@ -0,0 +1,7 @@
+﻿namespace Stocks.Core.DividendDisplay
+{
+    public interface ICalendarGenerator
+    {
+        IEnumerable<IEnumerable<DisplayDay>> GenerateMonth();
+    }
+}
