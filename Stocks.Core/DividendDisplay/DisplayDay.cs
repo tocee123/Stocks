eecb158ea6431ed447372d5f3cf9412d2f39eaca
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-07-05 18:40:36 +0200

    Make calendar days smaller (#43)
    * made the day smaller
    
    * New height
    
    * display all
    
    * format
    
    * style added
    
    * update style
    
    * update display
    
    * remove commented out code
    
    * updated style

--- Stocks.Core/DividendDisplay/DisplayDay.cs
+++ Stocks.Core/DividendDisplay/DisplayDay.cs
@@ -1,8 +1,8 @@
 ﻿namespace Stocks.Core.DividendDisplay;
-public record DisplayDay(int Day, string DayOfWeek, string CardCss, string HeaderCss, IEnumerable<DisplayDividendHistory> DisplayDividendHistories)
+public record DisplayDay(int Day, string DayOfWeek, string CardCss, string HeaderCss, string ContainerCss, IEnumerable<DisplayDividendHistory> DisplayDividendHistories)
 {
     public static DisplayDay ToDisplayDay(IDateProvider dateProvider, DateTime d, Dictionary<DateTime, List<DisplayDividendHistory>> dividendHistoriesByDate)
-    => new(d.Day, GetDisplayDateOfWeek(d), GetClassForCard(dateProvider, d), GetClassForHeader(dateProvider, d), dividendHistoriesByDate.GetValueOrDefault(d, new List<DisplayDividendHistory>()));
+    => new(d.Day, GetDisplayDateOfWeek(d), GetClassForCard(dateProvider, d), GetClassForHeader(dateProvider, d), GetClassForContainer(dateProvider,d), dividendHistoriesByDate.GetValueOrDefault(d, new List<DisplayDividendHistory>()));
 
     private static string GetDisplayDateOfWeek(DateTime day)
         => day.DayOfWeek.ToString()[..3];
@@ -13,6 +13,14 @@ public record DisplayDay(int Day, string DayOfWeek, string CardCss, string Heade
         { } when date.Month != dateProvider.GetToday().Month => "headerOtherMonth",
         { } when dateProvider.IsWeekend(date) => "headerWeekend",
         _ => "header",
+    };
+
+    private static string GetClassForContainer(IDateProvider dateProvider, DateTime date) => date switch
+    {
+        { } when date == dateProvider.GetToday() => "containerToday",
+        { } when date.Month != dateProvider.GetToday().Month => "containerOtherMonth",
+        { } when dateProvider.IsWeekend(date) => "containerWeekend",
+        _ => "container",
     };
 
     private static string GetClassForCard(IDateProvider dateProvider, DateTime date)
