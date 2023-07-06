cd910b9c7bc8e569fe230a02261019059a658c34
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-07-06 08:58:54 +0200

    Added name

--- Stocks.Core/DividendDisplay/DisplayDividendHistory.cs
+++ Stocks.Core/DividendDisplay/DisplayDividendHistory.cs
@@ -1,10 +1,10 @@
 ﻿namespace Stocks.Core.DividendDisplay;
 
-public record DisplayDividendHistory(DateTime Date, string Ticker, string Css, double Amount)
+public record DisplayDividendHistory(DateTime Date, string Ticker, string Name, string Css, double Amount)
 {
-    public static DisplayDividendHistory[] ToDisplayDividendHistories(string ticker, DividendHistory dividendHistory)
+    public static DisplayDividendHistory[] ToDisplayDividendHistories(StockDividend stock, DividendHistory dividendHistory)
     {
-        var ex = new DisplayDividendHistory(dividendHistory.ExDate, ticker, "exDate", dividendHistory.Amount);
+        var ex = new DisplayDividendHistory(dividendHistory.ExDate, stock.Ticker, stock.Name, "exDate", dividendHistory.Amount);
         var pay = ex with { Date = dividendHistory.PayDate, Css = "payDate" };
         return new[] { ex, pay };
     }
