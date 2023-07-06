8e80c4e7155ee95221123f86e1baff767395712d
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-07-06 15:07:16 +0200

    Add a range control for dividend yield (#52)
    * Added new control
    
    * Remove unused code
    
    * Continue work
    
    * Displaying by minimum dividend yield

--- Stocks.Core/DividendDisplay/DisplayDividendHistory.cs
+++ Stocks.Core/DividendDisplay/DisplayDividendHistory.cs
@@ -2,15 +2,16 @@
 
 namespace Stocks.Core.DividendDisplay;
 
-public record DisplayDividendHistory(DateTime Date, string Ticker, string Name, string Css, double Amount, IEnumerable<DisplayDividendHistoryDetail> Details)
+public record DisplayDividendHistory(DateTime Date, string Ticker, string Name, string Css, double Amount, double Yield, IEnumerable<DisplayDividendHistoryDetail> Details)
 {
     public static DisplayDividendHistory[] ToDisplayDividendHistories(StockDividend stock, DividendHistory dividendHistory)
     {
+        double yield = stock.DividendHistories.Where(dh => dh.PayDate.Year == dividendHistory.PayDate.Year).Sum(dh => dh.Amount) / stock.Price;
         var details = new List<DisplayDividendHistoryDetail> { 
         new("Current price:", stock.Price.ToDollarDisplay()),
         new("Current amount:", dividendHistory.Amount.ToDollarDisplay()),
         new("Dividend to price:", (dividendHistory.Amount/stock.Price).ToPercentageDisplay()),
-        new("Dividend yield:", (stock.DividendHistories.Where(dh=>dh.PayDate.Year == dividendHistory.PayDate.Year).Sum(dh=>dh.Amount)/stock.Price).ToPercentageDisplay()),
+        new("Dividend yield:", yield.ToPercentageDisplay()),
         };
 
         var characterToIndex = "(";
@@ -19,7 +20,7 @@ public record DisplayDividendHistory(DateTime Date, string Ticker, string Name, 
             ? stock.Name[..stock.Name.IndexOf(characterToIndex)]
             : stock.Name;
 
-        var ex = new DisplayDividendHistory(dividendHistory.ExDate, stock.Ticker, stockName, "exDate", dividendHistory.Amount, details);
+        var ex = new DisplayDividendHistory(dividendHistory.ExDate, stock.Ticker, stockName, "exDate", dividendHistory.Amount, yield, details);
         var pay = ex with { Date = dividendHistory.PayDate, Css = "payDate" };
         return new[] { ex, pay };
     }
