f4ee5b25d3c24bdd1784d1f86d6101ded993631c
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-07-12 22:57:23 +0200

    Added new types

---
+++ Stocks.Dal/Entities/Stock.cs
@@ -0,0 +1,7 @@
+﻿namespace Stocks.Dal.Entities;
+public class Stock
+{
+    public int Id { get; set; }
+    public string Ticker { get; set; }
+    public string Name { get; set; }
+}
---
+++ Stocks.Dal/Entities/StockContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Stocks.Dal.Entities;
+
+public class StockContext : DbContext
+{
+    public StockContext(DbContextOptions<StockContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Stock> Person { get; set; }
+}
