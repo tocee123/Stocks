16d51d1e2b981ffe83645bf32486ba83d0cfc2bb
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-07-12 23:38:08 +0200

    Added flow

--- Stocks.DividendUpdater/Program.cs
+++ Stocks.DividendUpdater/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using Stocks.Core.Loaders;
+using Stocks.Dal.Entities;
+
+var loader = new StockDividendHistoryLoader();
+var history = await loader.DownloadStockHistoryAsync("QYLD");
+var contextFactory = new StockContextFactory();
+using var context = contextFactory.CreateDbContext(null);
+var stock = new Stock { 
+Name = history.Name,
+Ticker = history.Ticker,
+StockDividends = history.
+};
