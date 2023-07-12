8fc79b5f50d01ee5eba2cb684ad54c92248b8850
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-07-12 23:56:39 +0200

    added all the tickers of interest

--- Stocks.DividendUpdater/Program.cs
+++ Stocks.DividendUpdater/Program.cs
@@ -1,23 +1,35 @@
 ﻿using Stocks.Core.Loaders;
 using Stocks.Dal.Entities;
 using StockDividendEntity = Stocks.Dal.Entities.StockDividend;
+using StockDividendCore = Stocks.Domain.Models.StockDividend;
+using Stocks.Core.Repositories;
 
 var loader = new StockDividendHistoryLoader();
-var history = await loader.DownloadStockHistoryAsync("QYLD");
+var stocksOfInterestRepository = new StocksOfInterestRespository();
+
+var histories = await Task.WhenAll(stocksOfInterestRepository.GetTickers().Select(async t => await loader.DownloadStockHistoryAsync(t)));
+
+
 var contextFactory = new StockContextFactory();
 using var context = contextFactory.CreateDbContext(null);
-var stock = new Stock
-{
-    Name = history.Name,
-    Ticker = history.Ticker,
-};
-stock.AddStockDividends(history.DividendHistories.Select(dh => new StockDividendEntity
-{
-    Amount = dh.Amount,
-    ExDividend = dh.ExDate,
-    PayoutDate = dh.PayDate,
-    Note = dh.Type
-}));
-stock.AddPrice(new StockPrice { Date = DateTime.Now, Price = history.Price });
-context.Stock.Add(stock);
+
+context.Stock.AddRange(histories.Select(ToStockEntity));
 context.SaveChanges();
+
+static Stock ToStockEntity(StockDividendCore history)
+{
+    var stock = new Stock
+    {
+        Name = history.Name,
+        Ticker = history.Ticker,
+    };
+    stock.AddStockDividends(history.DividendHistories.Select(dh => new StockDividendEntity
+    {
+        Amount = dh.Amount,
+        ExDividend = dh.ExDate,
+        PayoutDate = dh.PayDate,
+        Note = dh.Type
+    }));
+    stock.AddPrice(new StockPrice { Date = DateTime.Today, Price = history.Price });
+    return stock;
+}
