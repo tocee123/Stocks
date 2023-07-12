bd101ddb57d261460d26b0ff12482b04df523dd5
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-07-12 23:22:28 +0200

    Added new entity

--- Stocks.Dal/Entities/Stock.cs
+++ Stocks.Dal/Entities/Stock.cs
@@ -4,4 +4,6 @@ public class Stock
     public int Id { get; set; }
     public string Ticker { get; set; }
     public string Name { get; set; }
+
+    public ICollection<StockDividend> StockDividends { get; private set; } = new List<StockDividend>();
 }
