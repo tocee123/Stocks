7a4d75e7c3a2cebd3d6d0ccfa43f597a2b8a0c34
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-07-13 07:09:43 +0200

    Added logging

--- Stocks.DividendUpdater/Program.cs
+++ Stocks.DividendUpdater/Program.cs
@@ -1,19 +1,55 @@
-﻿using Stocks.Core.Loaders;
+﻿using Microsoft.Extensions.Logging;
+using Stocks.Core.Loaders;
 using Stocks.Core.Repositories;
 using Stocks.Dal.Entities;
 using StockDividendCore = Stocks.Domain.Models.StockDividend;
 using StockDividendEntity = Stocks.Dal.Entities.StockDividend;
 
+var logger = LoggerFactory.Create(builder =>
+{
+    builder.AddConsole();
+}).CreateLogger<Program>();
+
+logger.LogInformation("Hello");
 var loader = new StockDividendHistoryLoader();
 var stocksOfInterestRepository = new StocksOfInterestRespository();
 
-var histories = (await Task.WhenAll(stocksOfInterestRepository.GetTickers().Select(async t => await loader.DownloadStockHistoryAsync(t)))).Where(s => !string.IsNullOrEmpty(s.Ticker));
+var histories = (await Task.WhenAll(stocksOfInterestRepository.GetTickers().Take(10).Select(async t => await loader.DownloadStockHistoryAsync(t)))).Where(s => !string.IsNullOrEmpty(s.Ticker));
 
 
 var contextFactory = new StockContextFactory();
 using var context = contextFactory.CreateDbContext(null);
+var newStockEntities = histories.Select(ToStockEntity);
+foreach (var newStock in newStockEntities)
+{
+    logger.LogInformation($"Processing {newStock.Name}");
+    var firstStock = context.Stock.FirstOrDefault(s => s.Ticker == newStock.Ticker);
+    if (firstStock is null)
+    {
+        context.Stock.Add(newStock);
+    }
+    else
+    {
 
-context.Stock.AddRange(histories.Select(ToStockEntity));
+
+        //foreach (var newStockDividend in newStock.StockDividends)
+        //{
+        //    if (!context.StockDividend.Any(sd => sd.StockId == firstStock.Id && sd.ExDividend == newStockDividend.ExDividend && sd.PayoutDate == newStockDividend.PayoutDate))
+        //    {
+        //        firstStock.AddStockDividends(newStockDividend);
+        //    }
+        //}
+        //foreach (var newStockPrice in newStock.StockPrices)
+        //{
+        //    if (!context.StockPrice.Any(sd => sd.StockId == firstStock.Id && sd.Date == newStockPrice.Date))
+        //    {
+        //        firstStock.AddPrice(newStockPrice);
+        //    }
+        //}
+    }
+}
+
+context.Stock.AddRange();
 context.SaveChanges();
 
 static Stock ToStockEntity(StockDividendCore history)
@@ -29,7 +65,7 @@ static Stock ToStockEntity(StockDividendCore history)
         ExDividend = dh.ExDate,
         PayoutDate = dh.PayDate,
         Note = dh.Type
-    }));
+    }).ToArray());
     stock.AddPrice(new StockPrice { Date = DateTime.Today, Price = history.Price });
     return stock;
 }
