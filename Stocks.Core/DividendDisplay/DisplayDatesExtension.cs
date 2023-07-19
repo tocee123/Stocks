e451f5124d7fa9637fdfd42ccc0551667fc6248c
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-07-19 09:38:43 +0200

    Added extension

---
+++ Stocks.Core/DividendDisplay/DisplayDatesExtension.cs
@@ -0,0 +1,9 @@
+﻿namespace Stocks.Core.DividendDisplay; 
+
+public static class DisplayDatesExtension
+{
+    public static double GetMaximumYield(this IEnumerable<IEnumerable<DisplayDay>> displayDays)
+    {
+        return 0;
+    }
+}
