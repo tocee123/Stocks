aa98625dc96436095c084e50519d7edddce50bdb
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-07-19 09:47:59 +0200

    Added new test

--- Stocks.Core/DividendDisplay/DisplayDatesExtension.cs
+++ Stocks.Core/DividendDisplay/DisplayDatesExtension.cs
@@ -1,9 +1,9 @@
-﻿namespace Stocks.Core.DividendDisplay; 
+﻿namespace Stocks.Core.DividendDisplay;
 
 public static class DisplayDatesExtension
 {
-    public static double GetMaximumYield(this IEnumerable<IEnumerable<DisplayDay>> displayDays)
+    public static double GetMaximumYield(this IEnumerable<IEnumerable<DisplayDay>> month)
     {
-        return 0;
+        return month.SelectMany(m => m.SelectMany(x => x.DisplayDividendHistories)).Max(m => m.Yield) * 100;
     }
 }
--- Stocks.Tests/Core/DividendDisplay/DisplayDatesExtensionTests.cs
+++ Stocks.Tests/Core/DividendDisplay/DisplayDatesExtensionTests.cs
@@ -1,4 +1,6 @@
-﻿namespace Stocks.Test.Core.DividendDisplay;
+﻿using Stocks.Core.DividendDisplay;
+
+namespace Stocks.Test.Core.DividendDisplay;
 
 public class DisplayDatesExtensionTests
 {
@@ -6,6 +8,16 @@ public class DisplayDatesExtensionTests
     public void GetMaximumYield_ReturnsTheMaximumYield()
     {
         var fixture = new Fixture();
+        var month = fixture.CreateMany<IEnumerable<DisplayDay>>();
 
+        var displayDividendHistoriesYields = month.SelectMany(m => m.SelectMany(x => x.DisplayDividendHistories).Select(x => x.Yield));
+
+        foreach (var item in displayDividendHistoriesYields.OrderByDescending(x=>x))
+        {
+            Console.WriteLine(item);
+        }
+
+        var maxDividendYield = month.GetMaximumYield();
+        maxDividendYield.Should().Be(displayDividendHistoriesYields.OrderByDescending(x => x).First() * 100);
     }
 }
