873658fc108bff141bb83f48de4d810c7e1c57dc
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-07-19 09:38:47 +0200

    Added tests

---
+++ Stocks.Tests/Core/DividendDisplay/DisplayDatesExtensionTests.cs
@@ -0,0 +1,11 @@
+﻿namespace Stocks.Test.Core.DividendDisplay;
+
+public class DisplayDatesExtensionTests
+{
+    [Test]
+    public void GetMaximumYield_ReturnsTheMaximumYield()
+    {
+        var fixture = new Fixture();
+
+    }
+}
