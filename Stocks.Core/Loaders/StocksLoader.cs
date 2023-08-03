3b1bed8e9f907ff6715862389ef8493cc3c22388
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-08-03 21:29:12 +0200

    82 feature create options pattern for updater (#84)
    * Addinig settings
    
    * New files added
    
    * Added Program class
    
    * Rename
    
    * Added service collection initialization
    
    * Updated code
    
    * updating code
    
    * Moved files around
    
    * Updated logic
    
    * Updated logic
    
    * Updated logging

--- Stocks.Core/Loaders/StocksLoader.cs
+++ Stocks.Core/Loaders/StocksLoader.cs
@@ -1,20 +1,28 @@
-﻿namespace Stocks.Core.Loaders
+﻿using Microsoft.Extensions.Logging;
+
+namespace Stocks.Core.Loaders
 {
     public class StocksLoader : IStocksLoader
     {
         private readonly IStockDividendHistoryLoader _stockDividendHistoryLoader;
+        private readonly ILogger<StocksLoader> _logger;
 
-        public StocksLoader(IStockDividendHistoryLoader stockDividendHistoryLoader)
+        public StocksLoader(ILogger<StocksLoader> logger, IStockDividendHistoryLoader stockDividendHistoryLoader)
         {
             _stockDividendHistoryLoader = stockDividendHistoryLoader;
+            _logger = logger;
         }
 
         public async Task<IEnumerable<StockDividend>> GetStockDividendsAsync(IEnumerable<string> tickers)
         {
+            _logger.LogInformation($"Processing {tickers.Count()} tickers");
+
             var stocksToCheck = tickers.Distinct()
             .Select(_stockDividendHistoryLoader.DownloadStockHistoryAsync);
+
             var stocks = await Task.WhenAll(stocksToCheck);
             var orderedStocks = stocks.Where(s => s.IsCorrectlyDownloaded && s.LatestDividendHistory is not null).OrderByDescending(s => s.LatestDividendHistory.ExDate);
+
             return orderedStocks;
         }
     }
---
+++ Stocks.DividendUpdater/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace Stocks.DividendUpdater;
+public class Settings
+{
+    public const string SectionName = "ConnectionStrings";
+    public string StockWebDividendDB { get; set; }
+    public string Redis { get; set; }
+}
