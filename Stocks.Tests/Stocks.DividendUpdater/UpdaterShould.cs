8b870a5ad9e61b9bb739e51b5c65323500324f07
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-09-21 19:17:43 +0200

    Updated code (#91)

---
+++ Stocks.Tests/Stocks.DividendUpdater/UpdaterShould.cs
@@ -0,0 +1,23 @@
+﻿using StockDividendEntity = Stocks.Dal.Entities.StockDividend;
+
+namespace Stocks.Test.Stocks.DividendUpdater;
+
+[TestFixture]
+public class UpdaterShould
+{
+    [Test]
+    public void ReturnAtLeastOneEntity()
+    {
+        var fixture = new Fixture();
+        fixture.Customize<StockDividendEntity>(c => c.With(sd => sd.StockId, 111));
+
+        var dividendsInDb = fixture.CreateMany<StockDividendEntity>().ToArray();
+
+        var newDividends = new StockDividendEntity[dividendsInDb.Count() + 1];
+        Array.Copy(dividendsInDb, newDividends, dividendsInDb.Count());
+        newDividends[newDividends.Length - 1] = fixture.Create<StockDividendEntity>();
+
+        var toBeInserted = newDividends.Where(d => dividendsInDb.All(db => db.StockId == d.StockId && db.PayoutDate != d.PayoutDate));
+        toBeInserted.Count().Should().Be(1);
+    }
+}
