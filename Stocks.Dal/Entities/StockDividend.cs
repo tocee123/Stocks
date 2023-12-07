a75f7f6eee334daf2b6b445e86fe7873be793d23
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-12-07 16:54:22 +0100

    Added new migration

--- Stocks.Dal/Entities/StockDividend.cs
+++ Stocks.Dal/Entities/StockDividend.cs
@@ -8,5 +8,6 @@ public class StockDividend
     public DateTime ExDividend { get; set; }
     public DateTime PayoutDate { get; set; }
     public double Amount { get; set; }
-    public string  Note { get; set; }
+    public string Note { get; set; }
+    public bool IsDeleted { get; set; }
 }
---
+++ Stocks.Dal/Migrations/20231207155232_IsDeletedAdded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stocks.Dal.Migrations
+{
+    /// <inheritdoc />
+    public partial class IsDeletedAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "StockDividend",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "StockDividend");
+        }
+    }
+}
