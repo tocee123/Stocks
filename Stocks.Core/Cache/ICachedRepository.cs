fa843ff6b00cee164b3a2c79466ca1fdd33be517
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-12-25 19:00:15 +0100

    Added new logic to clear cache

--- Stocks.Core/Cache/ICachedRepository.cs
+++ Stocks.Core/Cache/ICachedRepository.cs
@@ -8,5 +8,6 @@ namespace Stocks.Core.Cache
         Task<string> GetStringAsync(string key);
         Task SetAsync<T>(string key, T value, CacheDuration cacheDuration = CacheDuration.OneHour);
         T Get<T>(string key);
+        Task Clear();
     }
 }
--- Stocks.Core/Cache/MemoryCashedRepository.cs
+++ Stocks.Core/Cache/MemoryCashedRepository.cs
@@ -1,35 +1,50 @@
 ﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Primitives;
 using Stocks.Core.Enums;
+using System.Threading;
 
-namespace Stocks.Core.Cache
+namespace Stocks.Core.Cache;
+public class MemoryCashedRepository : ICachedRepository
 {
-    public class MemoryCashedRepository : ICachedRepository
+    private readonly IMemoryCache _memoryCache = new MemoryCache(new MemoryCacheOptions());
+    private static CancellationTokenSource _resetCacheToken = new();
+
+    public async Task Clear()
     {
-        private readonly IMemoryCache _memoryCache = new MemoryCache(new MemoryCacheOptions());
-
-        public T Get<T>(string key)
+        await Task.Run(() =>
         {
-            return _memoryCache.Get<T>(key);
-        }
-
-        public async Task<T> GetAsync<T>(string key)
-        {
-            return await Task.Run(() => _memoryCache.Get<T>(key));
-        }
-
-        public async Task<string> GetStringAsync(string key)
-        {
-            return await Task.Run(() => _memoryCache.Get<string>(key));
-        }
-
-        public async Task SetAsync<T>(string key, T value, CacheDuration cacheDuration = CacheDuration.OneHour)
-        {
-            var cacheEntryOption = new MemoryCacheEntryOptions
+            if (_resetCacheToken != null && !_resetCacheToken.IsCancellationRequested && _resetCacheToken.Token.CanBeCanceled)
             {
-                AbsoluteExpirationRelativeToNow = cacheDuration.GetExpiration()
-            };
+                _resetCacheToken.Cancel();
+                _resetCacheToken.Dispose();
+            }
 
-            await Task.Run(() => _memoryCache.Set(key, value, cacheEntryOption));
-        }
+            _resetCacheToken = new CancellationTokenSource();
+        });
+    }
+
+    public T Get<T>(string key)
+    {
+        return _memoryCache.Get<T>(key);
+    }
+
+    public async Task<T> GetAsync<T>(string key)
+    {
+        return await Task.Run(() => _memoryCache.Get<T>(key));
+    }
+
+    public async Task<string> GetStringAsync(string key)
+    {
+        return await Task.Run(() => _memoryCache.Get<string>(key));
+    }
+
+    public async Task SetAsync<T>(string key, T value, CacheDuration cacheDuration = CacheDuration.OneHour)
+    {
+        var cacheEntryOption = new MemoryCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = cacheDuration.GetExpiration()
+        };
+        cacheEntryOption.AddExpirationToken(new CancellationChangeToken(_resetCacheToken.Token));
+        await Task.Run(() => _memoryCache.Set(key, value, cacheEntryOption));
     }
 }
