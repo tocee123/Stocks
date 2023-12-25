8117e92ef5def4bc48cabbb7f73783ec25c60306
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-12-25 19:01:59 +0100

    Added new logic to clear cache (#96)

--- Stocks.Core/Cache/OfflineCachedRepository.cs
+++ Stocks.Core/Cache/OfflineCachedRepository.cs
@@ -2,43 +2,45 @@
 using Stocks.Core.Enums;
 using System.IO;
 
-namespace Stocks.Core.Cache
+namespace Stocks.Core.Cache;
+public class OfflineCachedRepository : ICachedRepository
 {
+    string folderPath = @"c:\temp\";
+    private string CreateFileName(string key) => $"{folderPath}{key}.json";
 
-    public class OfflineCachedRepository : ICachedRepository
+    private string ReadFromCache(string key)
     {
-        string folderPath = @"c:\temp\";
-        private string CreateFileName(string key) => $"{folderPath}{key}.json";
+        var fileName = CreateFileName(key);
+        string value = null;
+        if (File.Exists(fileName))
+            value = File.ReadAllText(fileName);
+        return value;
+    }
 
-        private string ReadFromCache(string key)
-        {
-            var fileName = CreateFileName(key);
-            string value = null;
-            if (File.Exists(fileName))
-                value = File.ReadAllText(fileName);
-            return value;
-        }
+    public T Get<T>(string key)
+    {
+        var value = ReadFromCache(key);
+        return value is null ? default
+           : JsonConvert.DeserializeObject<T>(value);
+    }
 
-        public T Get<T>(string key)
-        {
-            var value = ReadFromCache(key);
-            return value is null ? default
-               : JsonConvert.DeserializeObject<T>(value);
-        }
+    public async Task<T> GetAsync<T>(string key)
+    {
+        return await Task.Run(() => Get<T>(key));
+    }
 
-        public async Task<T> GetAsync<T>(string key)
-        {
-            return await Task.Run(() => Get<T>(key));
-        }
+    public async Task<string> GetStringAsync(string key)
+    {
+        return await Task.Run(() => ReadFromCache(key));
+    }
 
-        public async Task<string> GetStringAsync(string key)
-        {
-            return await Task.Run(() => ReadFromCache(key));
-        }
+    public async Task SetAsync<T>(string key, T value, CacheDuration cacheDuration = CacheDuration.OneHour)
+    {
+        await File.WriteAllTextAsync(CreateFileName(key), JsonConvert.SerializeObject(value));
+    }
 
-        public async Task SetAsync<T>(string key, T value, CacheDuration cacheDuration = CacheDuration.OneHour)
-        {
-            await File.WriteAllTextAsync(CreateFileName(key), JsonConvert.SerializeObject(value));
-        }
+    public async Task Clear()
+    {
+        //do nothing yet
     }
 }
--- Stocks.Core/Cache/RedisCachedRepository.cs
+++ Stocks.Core/Cache/RedisCachedRepository.cs
@@ -3,47 +3,61 @@ using Newtonsoft.Json;
 using StackExchange.Redis;
 using Stocks.Core.Enums;
 
-namespace Stocks.Core.Cache
+namespace Stocks.Core.Cache;
+public class RedisCachedRepository : ICachedRepository
 {
-    public class RedisCachedRepository : ICachedRepository
+    readonly ConfigurationOptions _redisConnectionString;
+
+    public RedisCachedRepository(IOptions<Settings> settings)
     {
-        readonly string _redisConnectionString;
+        _redisConnectionString = ConfigurationOptions.Parse(settings.Value.Redis);
+        _redisConnectionString.ConnectRetry = 5;
+        _redisConnectionString.AllowAdmin = true;
+    }
 
-        public RedisCachedRepository(IOptions<Settings> settings)
-        {
-            _redisConnectionString = settings.Value.Redis;
-        }
-
-        public T Get<T>(string key)
-        {
-            using var cm = ConnectionMultiplexer.Connect(_redisConnectionString);
-            var db = cm.GetDatabase();
-            string value = db.StringGet(key);
-            return value is null ? default
-                : JsonConvert.DeserializeObject<T>(value);
-        }
-
-        public async Task<T> GetAsync<T>(string key)
-        {
-            var value = await GetStringAsync(key);
-            return value is null ? default
-                : JsonConvert.DeserializeObject<T>(value);
-        }
-
-        public async Task<string> GetStringAsync(string key)
+    public async Task Clear()
+    {
+        await Task.Run(() =>
         {
             using var cm = ConnectionMultiplexer.Connect(_redisConnectionString);
-            var db = cm.GetDatabase();
-            return await db.StringGetAsync(key);
-        }
+            var endpoints = cm.GetEndPoints(true);
+            foreach (var endpoint in endpoints)
+            {
+                var server = cm.GetServer(endpoint);
+                server.FlushAllDatabases();
+            }
+        });
+    }
 
-        public async Task SetAsync<T>(string key, T value, CacheDuration cacheDuration = CacheDuration.OneHour)
-        {
-            var expiry = cacheDuration.GetExpiration();
-            var convertedValue = JsonConvert.SerializeObject(value);
-            using var cm = ConnectionMultiplexer.Connect(_redisConnectionString);
-            var db = cm.GetDatabase();
-            await db.StringSetAsync(key, convertedValue, expiry);
-        }
+    public T Get<T>(string key)
+    {
+        using var cm = ConnectionMultiplexer.Connect(_redisConnectionString);
+        var db = cm.GetDatabase();
+        string value = db.StringGet(key);
+        return value is null ? default
+            : JsonConvert.DeserializeObject<T>(value);
+    }
+
+    public async Task<T> GetAsync<T>(string key)
+    {
+        var value = await GetStringAsync(key);
+        return value is null ? default
+            : JsonConvert.DeserializeObject<T>(value);
+    }
+
+    public async Task<string> GetStringAsync(string key)
+    {
+        using var cm = ConnectionMultiplexer.Connect(_redisConnectionString);
+        var db = cm.GetDatabase();
+        return await db.StringGetAsync(key);
+    }
+
+    public async Task SetAsync<T>(string key, T value, CacheDuration cacheDuration = CacheDuration.OneHour)
+    {
+        var expiry = cacheDuration.GetExpiration();
+        var convertedValue = JsonConvert.SerializeObject(value);
+        using var cm = ConnectionMultiplexer.Connect(_redisConnectionString);
+        var db = cm.GetDatabase();
+        await db.StringSetAsync(key, convertedValue, expiry);
     }
 }
