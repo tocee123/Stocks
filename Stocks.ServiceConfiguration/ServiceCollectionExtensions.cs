0758310f48ea16a5d63862b431155b1dd41f426f
tocee123 <93769005+tocee123@users.noreply.github.com>
2024-01-03 11:37:40 +0100

    Added new extensins for service collections

---
+++ Stocks.ServiceConfiguration/ServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Stocks.Dal;
+
+namespace Stocks.Infrastructure.Configurations;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddConfiguration(this IServiceCollection serviceCollection, IConfiguration configuration)
+    {
+        serviceCollection.Configure<DbAccess>(DbAccess.Redis, configuration.GetSection(DbAccess.Redis));
+        serviceCollection.Configure<DbAccess>(DbAccess.StockWebDividendDB, configuration.GetSection(DbAccess.StockWebDividendDB));
+
+        return serviceCollection;
+    }
+
+    public static IServiceCollection AddDbContext(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddDbContextFactory<StockContext, StockContextFactoryWithOptions>();
+        serviceCollection.AddTransient(sp =>
+        {
+            var cf = sp.GetRequiredService<IDbContextFactory<StockContext>>();
+
+            var db = cf.CreateDbContext();
+
+            return db;
+
+        });
+        return serviceCollection;
+    }
+}
